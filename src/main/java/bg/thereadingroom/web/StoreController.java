ec515e03db9b712afdbd8d4b6dbb91a537c2b36b
Georgi Bozhinov <ge.bozhinov@gmail.com>
2025-03-31 21:35:36 +0300

    config the openapi generator

--- src/main/java/bg/thereadingroom/web/StoreController.java
+++ src/main/java/bg/thereadingroom/web/StoreController.java
@@ -1,21 +1,18 @@
 package bg.thereadingroom.web;
 
-import bg.thereadingroom.web.StoreApi;
 import bg.thereadingroom.model.dto.ViewStoreDTO;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 
-@RestController
-@RequestMapping("api/store")
-@Tag(name = "Store", description = "Store management APIs")
-public class StoreController extends StoreApi {
+@Component
+public class StoreController implements StoreApiDelegate {
 
-    @GetMapping
-    @Operation(summary = "View store")
-    public ViewStoreDTO viewStore(Integer id) {
-        return new ViewStoreDTO();
+    @Override
+    public ResponseEntity<ViewStoreDTO> viewStore(Integer id) {
+
+        ViewStoreDTO viewStoreDTO = new ViewStoreDTO();
+        viewStoreDTO.setId(id);
+
+        return ResponseEntity.ok(viewStoreDTO);
     }
 }
