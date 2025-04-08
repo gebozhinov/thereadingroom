a693bd0fc4ca551e333cc442ef99f65b5809e1b5
Georgi Bozhinov <ge.bozhinov@gmail.com>
2025-04-08 21:42:55 +0300

    test3

--- src/test/java/bg/thereadingroom/integration/web/BookControllerIntegrationTest.java
+++ src/test/java/bg/thereadingroom/integration/web/BookControllerIntegrationTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class BookControllerIntegrationTest extends IntegrationTestInit {
 
     @Autowired
