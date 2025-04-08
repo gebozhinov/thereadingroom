be44f42569f1a30f466d8ee81813d0d3938c194a
Georgi Bozhinov <ge.bozhinov@gmail.com>
2025-04-08 21:45:01 +0300

    test4

--- src/test/java/bg/thereadingroom/integration/web/BookControllerIntegrationTest.java
+++ src/test/java/bg/thereadingroom/integration/web/BookControllerIntegrationTest.java
@@ -55,6 +55,6 @@ public class BookControllerIntegrationTest extends IntegrationTestInit {
 
         List<BookEntity> all = bookRepository.findAll();
 
-        assertThat(all.size()).isEqualTo(3);
+        assertThat(all.size()).isEqualTo(4);
     }
 }
